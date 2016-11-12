ee0d10ce2fb523ab04658e898813d97a785f97b1
mochechan <you@example.com>
2016-11-12 20:27:40 +0800

    up

--- rpicloud/rpi2/new.sh
+++ rpicloud/rpi2/new.sh
@@ -9,7 +9,7 @@ echo Installing docker
 cd ~
 
 echo This rpi2 has a rpi camera.
-git clone git@github.com:131/h264-live-player.git ~/h264-live-player
+git clone https://github.com/131/h264-live-player.git ~/h264-live-player
 
 
 # https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging?view=all
